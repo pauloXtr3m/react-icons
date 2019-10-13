98c2719d1a3e5277f34e0c4ea2274ba4c8133b8f
Dorival Pedroso <dorival.pedroso@gmail.com>
2019-10-13 22:34:13 +1000

    Improve gen code. Generate index

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,21 +1,13 @@
 import { sync as delSync } from 'del';
 import fs from 'fs-extra';
 import path from 'path';
-import runAll from './runAll';
-import { Icode } from './types';
+import { runAll } from './runAll';
 
 const DIST_PATH = path.join(__dirname, '..', 'dist');
 const BUILD_PATH = path.join(__dirname, '..', 'src');
 
-const build = () => {
-  delSync(DIST_PATH);
-  delSync(BUILD_PATH);
-  fs.mkdirSync(BUILD_PATH);
-  runAll('assets/*.svg', (code: Icode) => {
-    const fp = path.join(BUILD_PATH, code.filepath);
-    fs.outputFile(fp, code.code);
-  });
-  fs.copyFileSync('scripts/extra/withClasses.tsx', BUILD_PATH + '/withClasses.tsx');
-};
-
-build();
+delSync(DIST_PATH);
+delSync(BUILD_PATH);
+fs.mkdirSync(BUILD_PATH);
+runAll('assets/*.svg');
+fs.copyFileSync('scripts/extra/withClasses.tsx', BUILD_PATH + '/withClasses.tsx');
--- scripts/runAll.ts
+++ scripts/runAll.ts
@@ -3,13 +3,11 @@ import glob from 'glob';
 import path from 'path';
 import SVGO from 'svgo';
 import { getSvgDims } from './helpers';
-import { Icode } from './types';
 import genCode from './genCode';
 
-const svgo = new SVGO({});
+const BUILD_PATH = path.join(__dirname, '..', 'src');
 
-// get the icon name
-const getName = (filepath: string) => path.basename(filepath, path.extname(filepath));
+const svgo = new SVGO({});
 
 // optimize file
 const handleFile = async (filepath: string, data: string) => {
@@ -24,24 +22,29 @@ const handleFile = async (filepath: string, data: string) => {
   }
 };
 
-// build the optimized SVG data
-const runAll = (globPattern: string, callback: (code: Icode) => void) => {
-  const filepaths = glob.sync(globPattern);
-  filepaths.forEach(filepath => {
-    fs.readFile(filepath, 'utf8', async (err, data) => {
-      if (err) {
-        throw err;
-      }
-      try {
-        const res = await handleFile(filepath, data);
-        if (res) {
-          callback(res);
-        }
-      } catch (error) {
-        console.error('handleFile failed');
-      }
-    });
-  });
-};
+// get the icon name
+export const getName = (filepath: string) => path.basename(filepath, path.extname(filepath));
 
-export default runAll;
+// build the optimized SVG data
+export const runAll = (globPattern: string) => {
+  const filepaths = glob.sync(globPattern);
+
+  filepaths.forEach(async filepath => {
+    try {
+      const data = fs.readFileSync(filepath, 'utf8');
+      const res = await handleFile(filepath, data);
+      if (res) {
+        const fp = path.join(BUILD_PATH, res.filepath);
+        fs.outputFileSync(fp, res.code);
+      } else {
+        throw new Error('handleFile failed');
+      }
+    } catch (error) {
+      console.warn('ERROR: ' + error);
+    }
+  });
+
+  const idx = filepaths.reduce((acc, curr) => `${acc}export * from './${getName(curr)}';\n`, '');
+  const fpidx = path.join(BUILD_PATH, 'index.ts');
+  fs.outputFileSync(fpidx, idx);
+};
